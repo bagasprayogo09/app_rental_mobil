59c9f1d04fd2ccfb4539d0b75027236b0aa8db8f
bagasprayogo09 <bagazprayogo09@gmail.com>
2023-11-30 15:48:48 +0700

    menambahkan  app_rental_mobil

---
+++ app_rental_mobil/application/views/templates_admin/footer.php
@@ -0,0 +1,30 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+    </div>
+    <div class="footer-right">
+
+    </div>
+</footer>
+</div>
+</div>
+
+<!-- General JS Scripts -->
+<script src="<?php echo base_url('assets/assets_stisla/dist/assets/modules/jquery.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/assets_stisla/dist/assets/modules/popper.js') ?>"></script>
+<script src="<?php echo base_url('assets/assets_stisla/dist/assets/modules/tooltip.js') ?>"></script>
+<script src="<?php echo base_url('assets/assets_stisla/dist/assets/modules/bootstrap/js/bootstrap.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/assets_stisla/dist/assets/modules/nicescroll/jquery.nicescroll.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/assets_stisla/dist/assets/modules/moment.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/assets_stisla/dist/assets/js/stisla.js') ?>"></script>
+
+
+<!-- Page Specific JS File -->
+<script src="<?php echo base_url('assets/assets_stisla/dist/assets/js/page/index-0.js') ?>"></script>
+
+<!-- Template JS File -->
+<script src="<?php echo base_url('assets/assets_stisla/dist/assets/js/scripts.js') ?>"></script>
+<script src="<?php echo base_url('assets/assets_stisla/dist/assets/js/custom.js') ?>"></script>
+</body>
+
+</html>
---
+++ app_rental_mobil/database/rental_mobil.sql
@@ -0,0 +1,216 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 03, 2022 at 05:46 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rental_mobil`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `nama_admin` varchar(120) NOT NULL,
+  `username` varchar(120) NOT NULL,
+  `password` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id_customer` int(11) NOT NULL,
+  `nama` varchar(120) NOT NULL,
+  `username` varchar(120) NOT NULL,
+  `alamat` varchar(120) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `no_telepon` varchar(20) NOT NULL,
+  `no_ktp` varchar(50) NOT NULL,
+  `password` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mobil`
+--
+
+CREATE TABLE `mobil` (
+  `id_mobil` int(11) NOT NULL,
+  `kode_type` varchar(120) NOT NULL,
+  `merk` varchar(120) NOT NULL,
+  `no_plat` varchar(20) NOT NULL,
+  `warna` varchar(20) NOT NULL,
+  `tahun` varchar(4) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `gambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mobil`
+--
+
+INSERT INTO `mobil` (`id_mobil`, `kode_type`, `merk`, `no_plat`, `warna`, `tahun`, `status`, `gambar`) VALUES
+(13, 'SDN', 'Pagani Zonda', 'B 1144 AND', 'HITAM', '2020', '0', 'wallhaven-472ldv_1000x1000.png'),
+(14, 'SDN', 'Lamborghini', 'B 1145 AND', 'ABU-ABU', '2020', '1', 'wallhaven-nk7ll6_1000x1000.png'),
+(16, 'SDN', 'Buggati', 'B 1146 AND', 'HITAM', '2020', '1', 'wallhaven-dg5llm_1000x1000.png'),
+(17, 'SDN', 'Nissan GT', 'B 1149 AND', 'PUTIH', '2020', '1', 'wallhaven-vm98o5_1000x1000.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rental`
+--
+
+CREATE TABLE `rental` (
+  `id_rental` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `tanggal_rental` date NOT NULL,
+  `tanggal_kembali` date NOT NULL,
+  `tanggal_pengembalian` date NOT NULL,
+  `status_rental` varchar(50) NOT NULL,
+  `status_pengembalian` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_rental` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `id_mobil` int(11) NOT NULL,
+  `tanggal_rental` date NOT NULL,
+  `tanggal_kembali` date NOT NULL,
+  `tanggal_pengembalian` date NOT NULL,
+  `status_pengembalian` varchar(50) NOT NULL,
+  `status_rental` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type`
+--
+
+CREATE TABLE `type` (
+  `id_type` int(11) NOT NULL,
+  `kode_type` varchar(10) NOT NULL,
+  `nama_type` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` (`id_type`, `kode_type`, `nama_type`) VALUES
+(1, 'SDN', 'Sedan');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id_customer`);
+
+--
+-- Indexes for table `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`id_mobil`);
+
+--
+-- Indexes for table `rental`
+--
+ALTER TABLE `rental`
+  ADD PRIMARY KEY (`id_rental`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_rental`);
+
+--
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id_type`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id_customer` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mobil`
+--
+ALTER TABLE `mobil`
+  MODIFY `id_mobil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `rental`
+--
+ALTER TABLE `rental`
+  MODIFY `id_rental` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_rental` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `id_type` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
